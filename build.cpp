4e42a8cdb88804db12dc87bd1f7a7b84f475b478
Eduardo Costa <m4c0@users.noreply.github.com>
2023-07-06 07:44:19 +0100

    poc: basic output infra
    this will enable a visualisation of the R-tree node - it will be more
    important than unit tests themselves

--- build.cpp
+++ build.cpp
@@ -1,9 +1,30 @@
 #include "../ecow/ecow.hpp"
 
+class pocify : public ecow::unit {
+  void build_self() const override {
+    std::cerr << "running poc" << std::endl;
+
+    const auto out = ecow::impl::current_target()->build_path() / "poc.html";
+    const auto cmd = name() + " " + out.string();
+    if (std::system(cmd.c_str()) != 0)
+      throw std::runtime_error("Proof-of-concept failed");
+  }
+
+  void create_self_cdb(std::ostream &) const override {}
+  [[nodiscard]] pathset self_objects() const override { return {}; }
+
+public:
+  using unit::unit;
+};
+
 int main(int argc, char **argv) {
   using namespace ecow;
 
+  auto poc = unit::create<tool>("poc");
+  poc->add_unit("poc");
+
   auto all = unit::create<seq>("all");
-  all->add_unit<>("poc");
+  all->add_ref(poc);
+  all->add_unit<pocify>(poc->executable().string());
   return run_main(all, argc, argv);
 }
--- poc.cpp
+++ poc.cpp
@@ -1,1 +1,14 @@
-int main() {}
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  if (argc != 2)
+    return 1;
+
+  FILE *out = fopen(argv[1], "w");
+  if (!out)
+    return 2;
+
+  fprintf(out, "<!DOCTYPE html>");
+  fclose(out);
+}
