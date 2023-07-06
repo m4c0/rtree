56ed73e014f8b9d6161ddfe0baf9b596b7dd62f9
Eduardo Costa <m4c0@users.noreply.github.com>
2023-07-06 08:20:11 +0100

    reading the csv

--- poc.cpp
+++ poc.cpp
@@ -15,16 +15,29 @@ public:
   void insert(unsigned id, aabb area) {}
 };
 
+void run_poc(FILE *in, FILE *out) {
+  tree t{};
+
+  unsigned qkm{};
+  unsigned lat{};
+  unsigned lng{};
+  while (fscanf(in, "%d,%d,%d\n", &qkm, &lat, &lng) == 3) {
+    fprintf(out, "<!-- %d %d %d -->\n", qkm, lat, lng);
+
+    // We could use the PLZ, but meh... forgot about it in the cleanup...
+    // t.insert(i, area);
+  }
+  // fprintf(out, "<rect x=50 y=50 width=100 height=200 "
+  //              "style='stroke:black;fill:transparent'>\n");
+}
+
 int main(int argc, char **argv) {
   if (argc != 2)
     return 1;
 
-  tree t{};
-
-  for (auto i = 0; i < 10240; i++) {
-    // We could use the PLZ, but meh... forgot about it in the cleanup...
-    // t.insert(i, area);
-  }
+  FILE *in = fopen("data.csv", "r");
+  if (!in)
+    return 2;
 
   FILE *out = fopen(argv[1], "w");
   if (!out)
@@ -32,8 +45,7 @@ int main(int argc, char **argv) {
 
   fprintf(out, "<!DOCTYPE html>\n");
   fprintf(out, "<html><body><svg width=800 height=800>\n");
-  fprintf(out, "<rect x=50 y=50 width=100 height=200 "
-               "style='stroke:black;fill:transparent'>\n");
+  run_poc(in, out);
   fprintf(out, "</svg></body></html>\n");
   fclose(out);
 }
